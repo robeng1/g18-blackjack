84048b86dc387c8476ebf0bdbd4a06cd7ec25cf8
Romeo <51507372+robeng1@users.noreply.github.com>
2021-11-19 14:54:11 +0000

    highly untested

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,4 +1,22 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GameTest {
-  
+
+    @Test
+    void makeWinner() {
+    }
+
+    @Test
+    void generateGameDeck() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void runGameLoop() {
+    }
 }
